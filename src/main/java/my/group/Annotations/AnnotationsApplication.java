a967df9a747cfd62cf4832b70728b01bc0a12f78
Lindenjefferson <lindenjeffersom@hotmail.com>
2020-06-08 16:34:06 -0300

    Initial commit

---
+++ src/main/java/my/group/Annotations/AnnotationsApplication.java
@@ -0,0 +1,13 @@
+package my.group.Annotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnotationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/group/Annotations/AnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package my.group.Annotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
